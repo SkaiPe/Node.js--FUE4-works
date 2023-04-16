3b1906d3f2e719f1b8306627f2e832ef6dd578ba
Skaidre Petraviciene <skaidreju@gmail.com>
2023-04-16 22:34:13 +0300

    atnaujink

---
+++ 1_paskaitaV1/index.js
@@ -0,0 +1,20 @@
+//console.log('Hello from V2');
+const express = require('express');// expers modulio importavimas
+const app = express();//aplikacijos uskurimas
+const port = 3000;//porto kanalo skai2ius
+
+//routas kelias /path
+//get =gra=ink duomenis
+app.get('/', (req, res)=>{
+    //req     - request kas ateina is i=ores, res = response  kas ateina is vidaus
+    res.send('Mano vardas Tomas'); //send metodas i6siun2ia duomenis
+});
+
+app.get('/today', (req, res)=>{
+    res.send(new Date().toDateString());
+});
+
+//serverio paleidimas
+app.listen(port, () => {
+    console.log('Server is listening on port ${port}');
+});
