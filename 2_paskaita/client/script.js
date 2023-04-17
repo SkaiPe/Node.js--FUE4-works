64f70d4607c771f1d1fc2680d291d8bd553cb053
Skaidre Petraviciene <skaidreju@gmail.com>
2023-04-17 20:52:23 +0300

    Pataisyta

--- 2_paskaita/client/script.js
+++ 2_paskaita/client/script.js
@@ -1,12 +1,28 @@
-const_nameButton = document.getElementById('#nameButton');
- nameButton.addEventListener('click', ()=>{
-    const name = document.querySelector('input[name='name'] ').value;
-     console.log(name);
+
+
     fetch("http://localhost:3000/")
-    .then((resp) => resp.json())
-    .then((response) => {​​
-      console.log(response);
-    }​​)
-    .catch((error) => {​​
-      console.log(error);
-    }​​);
+  .then((resp) => resp.json())
+  .then((response) => {
+    const namesList = document.getElementById("names");
+
+    response.forEach((name) => {
+      const li = document.createElement("li");
+      li.textContent = name;
+      namesList.append(li);
+    });
+  });
+
+const nameButton = document.getElementById("nameButton");
+nameButton.addEventListener("click", () => {
+  const name = document.querySelector("input[name='name']").value;
+
+  fetch("http://localhost:3000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name }),
+  }).then(() => {
+    location.reload();
+  });
+});
